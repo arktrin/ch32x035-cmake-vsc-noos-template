8c138377e1d0f4d339c459b9a63af7bf9e76eac9
arktrin <who@whatever.xyz>
2024-09-15 23:12:58 +0300

    initial i2c test
    + update HAL

--- src/main.c
+++ src/main.c
@@ -1,21 +1,87 @@
+/********************************** (C) COPYRIGHT *******************************
+ * File Name          : main.c
+ * Author             : WCH
+ * Version            : V1.0.0
+ * Date               : 2023/04/06
+ * Description        : Main program body.
+ *********************************************************************************
+ * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+ * Attention: This software (modified or not) and binary are used for 
+ * microcontroller manufactured by Nanjing Qinheng Microelectronics.
+ *******************************************************************************/
+
 /*
  *@Note
- *USART1_Tx(PB10) is used as a print debug port output
- *GPIO pins PA0 and PA1 are toggling
+ *7-bit addressing mode, master/slave mode, transceiver routine:
+ *I2C1_SCL(PA10)\I2C1_SDA(PA11).
+ *This routine demonstrates that Master sends and Slave receives.
+ *Note: The two boards download the Master and Slave programs respectively,
+ *and power on at the same time.During the I2C communication process, 
+ *the pins are open drain outputs. 
+ *    Hardware connection:PA10 -- PA10
+ *                        PA11 -- PA11
  *
  */
 
 #include "ch32x035.h"
 #include "debug.h"
 
-void LED1_BLINK_INIT(void)
+/* I2C Mode Definition */
+#define HOST_MODE   0
+#define SLAVE_MODE   1
+
+/* I2C Communication Mode Selection */
+#define I2C_MODE   HOST_MODE
+//#define I2C_MODE   SLAVE_MODE
+
+/* Global define */
+#define Size   6
+#define RXAdderss   0x02
+#define TxAdderss   0x02
+
+/* Global Variable */
+u8 TxData[Size] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 };
+u8 RxData[5][Size];
+
+/*********************************************************************
+ * @fn      IIC_Init
+ *
+ * @brief   Initializes the IIC peripheral.
+ *
+ * @return  none
+ */
+void IIC_Init(u32 bound, u16 address)
 {
-    GPIO_InitTypeDef GPIO_InitStructure = { 0 };
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+	GPIO_InitTypeDef GPIO_InitStructure={0};
+	I2C_InitTypeDef I2C_InitTSturcture={0};
+
+    RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOA, ENABLE );
+    RCC_APB1PeriphClockCmd( RCC_APB1Periph_I2C1, ENABLE );
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    GPIO_Init( GPIOA, &GPIO_InitStructure );
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init( GPIOA, &GPIO_InitStructure );
+
+	I2C_InitTSturcture.I2C_ClockSpeed = bound;
+	I2C_InitTSturcture.I2C_Mode = I2C_Mode_I2C;
+	I2C_InitTSturcture.I2C_DutyCycle = I2C_DutyCycle_16_9;
+	I2C_InitTSturcture.I2C_OwnAddress1 = address;
+	I2C_InitTSturcture.I2C_Ack = I2C_Ack_Enable;
+	I2C_InitTSturcture.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
+    I2C_Init( I2C1, &I2C_InitTSturcture );
+
+	I2C_Cmd( I2C1, ENABLE );
+
+#if (I2C_MODE == HOST_MODE)
+	I2C_AcknowledgeConfig( I2C1, ENABLE );
+
+#endif
 }
 
 /*********************************************************************
@@ -27,25 +93,73 @@ void LED1_BLINK_INIT(void)
  */
 int main(void)
 {
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+    u8 i = 0;
+	u8 j = 0;
+	u8 p = 0;
     SystemCoreClockUpdate();
     Delay_Init();
-    USART_Printf_Init(115200);
-
-    printf("SystemClk:%d\r\n", SystemCoreClock);
+    USART_Printf_Init( 460800 );
+    printf( "SystemClk:%d\r\n", SystemCoreClock );
     printf( "ChipID:%08x\r\n", DBGMCU_GetCHIPID() );
-    printf("This is printf example\r\n");
 
-    u8 i = 0;
-    u8 j = 1;
-    LED1_BLINK_INIT();
-    GPIO_ResetBits(GPIOA, GPIO_Pin_0 | GPIO_Pin_1);
+#if (I2C_MODE == HOST_MODE)
+    printf("IIC Host mode\r\n");
+	IIC_Init( 80000, TxAdderss);
 
-    while(1)
+	for( j =0; j < 5; j++)
+	 {
+    while( I2C_GetFlagStatus( I2C1, I2C_FLAG_BUSY ) != RESET );
+
+	I2C_GenerateSTART( I2C1, ENABLE );
+
+	while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_MODE_SELECT ) );
+	I2C_Send7bitAddress( I2C1, 0x02, I2C_Direction_Transmitter );
+
+    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED ) );
+ 
+    for( i=0; i< 6;i++ )
     {
-        Delay_Ms(250);
-        GPIO_WriteBit(GPIOA, GPIO_Pin_0, (i == 0) ? (i = Bit_SET) : (i = Bit_RESET));
-        Delay_Ms(250);
-        GPIO_WriteBit(GPIOA, GPIO_Pin_1, (j == 0) ? (j = Bit_SET) : (j = Bit_RESET));
+               while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTING ) );
+               I2C_SendData( I2C1, TxData[i] );
     }
+
+    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED ) );
+    I2C_GenerateSTOP( I2C1, ENABLE );
+	Delay_Ms(1000);
+	 }
+
+#elif (I2C_MODE == SLAVE_MODE)
+	printf("IIC Slave mode\r\n");
+	IIC_Init( 80000, RXAdderss);
+
+	for( p =0; p < 5; p++)
+	{
+	
+    i = 0;
+	while( !I2C_CheckEvent( I2C1, I2C_EVENT_SLAVE_RECEIVER_ADDRESS_MATCHED ) );
+    while( i < 6 )
+    {
+        if( I2C_GetFlagStatus( I2C1, I2C_FLAG_RXNE ) !=  RESET )
+        {
+            RxData[p][i] = I2C_ReceiveData( I2C1 );
+            i++;
+        }
+    }
+    while( !I2C_CheckEvent( I2C1, I2C_EVENT_SLAVE_STOP_DETECTED ) );
+        I2C1->CTLR1 &= I2C1->CTLR1;
+    }
+	    printf( "RxData:\r\n" );
+	 for(p=0; p<5; p++)
+   {
+        for( i = 0; i < 6; i++ )
+		{
+           printf( "%02x ", RxData[p][i] );
+		}
+		   printf( "\r\n ");
+	 }
+	 
+ 
+#endif
+
+	while(1);
 }
