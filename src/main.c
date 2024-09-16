ffd781d3f4cb430ceab7cd5d81dfcd41de73c58f
arktrin <who@whatever.xyz>
2024-09-16 10:38:17 +0300

    test

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
  * File Name          : main.c
  * Author             : WCH
  * Version            : V1.0.0
- * Date               : 2023/04/06
+ * Date               : 2024/06/05
  * Description        : Main program body.
  *********************************************************************************
  * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
@@ -12,76 +12,114 @@
 
 /*
  *@Note
- *7-bit addressing mode, master/slave mode, transceiver routine:
- *I2C1_SCL(PA10)\I2C1_SDA(PA11).
- *This routine demonstrates that Master sends and Slave receives.
- *Note: The two boards download the Master and Slave programs respectively,
- *and power on at the same time.During the I2C communication process, 
- *the pins are open drain outputs. 
- *    Hardware connection:PA10 -- PA10
- *                        PA11 -- PA11
+ *two-wire full duplex mode, master/slave mode, data transceiver:
+ *Master:SPI1_SCK(PA5)\SPI1_MISO(PA6)\SPI1_MOSI(PA7).
+ *Slave:SPI1_SCK(PA5)\SPI1_MISO(PA6)\SPI1_MOSI(PA7).
  *
+ *This example demonstrates simultaneous full-duplex transmission and
+ *reception between Master and Slave.
+ *Note: The two boards download the Master and Slave programs respectively,
+ *and power on at the same time.
+ *    Hardware connection:PA5 -- PA5
+ *                        PA6 -- PA6
+ *                        PA7 -- PA7
+ *When using SPI slave mode to send data:
+ *  -the CPOL bit should be set to 1
+ *  -the data should be sent using spi mode 2 or spi mode 3.
  */
 
 #include "ch32x035.h"
 #include "debug.h"
+#include "string.h"
 
-/* I2C Mode Definition */
+/* SPI Mode Definition */
 #define HOST_MODE   0
 #define SLAVE_MODE   1
 
-/* I2C Communication Mode Selection */
-#define I2C_MODE   HOST_MODE
-//#define I2C_MODE   SLAVE_MODE
+/* SPI Communication Mode Selection */
+#define SPI_MODE   HOST_MODE
+// #define SPI_MODE   SLAVE_MODE
 
 /* Global define */
-#define Size   6
-#define RXAdderss   0x02
-#define TxAdderss   0x02
+#define Size 18
 
 /* Global Variable */
-u8 TxData[Size] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 };
-u8 RxData[5][Size];
+
+u16 TxData[Size] = { 0x0101, 0x0202, 0x0303, 0x0404, 0x0505, 0x0606,
+                     0x1111, 0x1212, 0x1313, 0x1414, 0x1515, 0x1616,
+                     0x2121, 0x2222, 0x2323, 0x2424, 0x2525, 0x2626 };
+u16 RxData[Size];
 
 /*********************************************************************
- * @fn      IIC_Init
+ * @fn      SPI_FullDuplex_Init
  *
- * @brief   Initializes the IIC peripheral.
+ * @brief   Configuring the SPI for full-duplex communication.
  *
  * @return  none
  */
-void IIC_Init(u32 bound, u16 address)
+void SPI_FullDuplex_Init(void)
 {
 	GPIO_InitTypeDef GPIO_InitStructure={0};
-	I2C_InitTypeDef I2C_InitTSturcture={0};
+	SPI_InitTypeDef SPI_InitStructure={0};
 
-    RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOA, ENABLE );
-    RCC_APB1PeriphClockCmd( RCC_APB1Periph_I2C1, ENABLE );
+	RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOA | RCC_APB2Periph_SPI1, ENABLE );
 
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init( GPIOA, &GPIO_InitStructure );
+#if (SPI_MODE == HOST_MODE)
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_2;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init( GPIOA, &GPIO_InitStructure );
 
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init( GPIOA, &GPIO_InitStructure );
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init( GPIOA, &GPIO_InitStructure );
 
-	I2C_InitTSturcture.I2C_ClockSpeed = bound;
-	I2C_InitTSturcture.I2C_Mode = I2C_Mode_I2C;
-	I2C_InitTSturcture.I2C_DutyCycle = I2C_DutyCycle_16_9;
-	I2C_InitTSturcture.I2C_OwnAddress1 = address;
-	I2C_InitTSturcture.I2C_Ack = I2C_Ack_Enable;
-	I2C_InitTSturcture.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
-    I2C_Init( I2C1, &I2C_InitTSturcture );
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init( GPIOA, &GPIO_InitStructure );
 
-	I2C_Cmd( I2C1, ENABLE );
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init( GPIOA, &GPIO_InitStructure );
 
-#if (I2C_MODE == HOST_MODE)
-	I2C_AcknowledgeConfig( I2C1, ENABLE );
+#elif (SPI_MODE == SLAVE_MODE)
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init( GPIOA, &GPIO_InitStructure );
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init( GPIOA, &GPIO_InitStructure );
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init( GPIOA, &GPIO_InitStructure );
 
 #endif
+
+	SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
+
+#if (SPI_MODE == HOST_MODE)
+	SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
+
+#elif (SPI_MODE == SLAVE_MODE)
+	SPI_InitStructure.SPI_Mode = SPI_Mode_Slave;
+
+#endif
+
+	SPI_InitStructure.SPI_DataSize = SPI_DataSize_16b;
+	SPI_InitStructure.SPI_CPOL = SPI_CPOL_High;
+	SPI_InitStructure.SPI_CPHA = SPI_CPHA_2Edge;
+	SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
+	SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_64;
+	SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
+	SPI_InitStructure.SPI_CRCPolynomial = 7;
+	SPI_Init( SPI1, &SPI_InitStructure );
+
+	SPI_Cmd( SPI1, ENABLE );
 }
 
 /*********************************************************************
@@ -93,73 +131,74 @@ void IIC_Init(u32 bound, u16 address)
  */
 int main(void)
 {
-    u8 i = 0;
-	u8 j = 0;
-	u8 p = 0;
-    SystemCoreClockUpdate();
-    Delay_Init();
-    USART_Printf_Init( 460800 );
-    printf( "SystemClk:%d\r\n", SystemCoreClock );
-    printf( "ChipID:%08x\r\n", DBGMCU_GetCHIPID() );
+	u8 i=0;
+	u8 j=1;
+	u8 value;
 
-#if (I2C_MODE == HOST_MODE)
-    printf("IIC Host mode\r\n");
-	IIC_Init( 80000, TxAdderss);
+	SystemCoreClockUpdate();
+	Delay_Init();
+    GPIO_ResetBits(GPIOA, GPIO_Pin_0 | GPIO_Pin_2);
+	USART_Printf_Init(460800);
+	printf("SystemClk:%d\r\n",SystemCoreClock);
+	printf( "ChipID:%08x\r\n", DBGMCU_GetCHIPID() );
 
-	for( j =0; j < 5; j++)
-	 {
-    while( I2C_GetFlagStatus( I2C1, I2C_FLAG_BUSY ) != RESET );
-
-	I2C_GenerateSTART( I2C1, ENABLE );
-
-	while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_MODE_SELECT ) );
-	I2C_Send7bitAddress( I2C1, 0x02, I2C_Direction_Transmitter );
-
-    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED ) );
- 
-    for( i=0; i< 6;i++ )
-    {
-               while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTING ) );
-               I2C_SendData( I2C1, TxData[i] );
-    }
-
-    while( !I2C_CheckEvent( I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED ) );
-    I2C_GenerateSTOP( I2C1, ENABLE );
+#if (SPI_MODE == SLAVE_MODE)
+  printf("Slave Mode\r\n");
 	Delay_Ms(1000);
-	 }
 
-#elif (I2C_MODE == SLAVE_MODE)
-	printf("IIC Slave mode\r\n");
-	IIC_Init( 80000, RXAdderss);
-
-	for( p =0; p < 5; p++)
-	{
-	
-    i = 0;
-	while( !I2C_CheckEvent( I2C1, I2C_EVENT_SLAVE_RECEIVER_ADDRESS_MATCHED ) );
-    while( i < 6 )
-    {
-        if( I2C_GetFlagStatus( I2C1, I2C_FLAG_RXNE ) !=  RESET )
-        {
-            RxData[p][i] = I2C_ReceiveData( I2C1 );
-            i++;
-        }
-    }
-    while( !I2C_CheckEvent( I2C1, I2C_EVENT_SLAVE_STOP_DETECTED ) );
-        I2C1->CTLR1 &= I2C1->CTLR1;
-    }
-	    printf( "RxData:\r\n" );
-	 for(p=0; p<5; p++)
-   {
-        for( i = 0; i < 6; i++ )
-		{
-           printf( "%02x ", RxData[p][i] );
-		}
-		   printf( "\r\n ");
-	 }
-	 
- 
 #endif
 
-	while(1);
+	SPI_FullDuplex_Init();
+
+#if (SPI_MODE == HOST_MODE)
+  printf("Host Mode\r\n");
+	Delay_Ms(2000);
+
+#endif
+
+	while(1)
+	{
+        Delay_Ms(250);
+        GPIO_WriteBit(GPIOA, GPIO_Pin_0, (i == 0) ? (i = Bit_SET) : (i = Bit_RESET));
+        Delay_Ms(250);
+        GPIO_WriteBit(GPIOA, GPIO_Pin_2, (j == 0) ? (j = Bit_SET) : (j = Bit_RESET));
+        // GPIO_WriteBit(GPIOA, GPIO_Pin_2, (i == 0) ? (i = Bit_SET) : (i = Bit_RESET));
+		// while( ( i<18 ) )
+		// {
+		// 	if( i<18 )
+		// 	{
+		// 		if( SPI_I2S_GetFlagStatus( SPI1, SPI_I2S_FLAG_TXE ) != RESET )
+		// 		{
+        //             GPIO_WriteBit(GPIOA, GPIO_Pin_2, (i == 0) ? (i = Bit_SET) : (i = Bit_RESET));
+		// 			SPI_I2S_SendData( SPI1, TxData[i] );
+        //             GPIO_WriteBit(GPIOA, GPIO_Pin_2, (i == 0) ? (i = Bit_SET) : (i = Bit_RESET));
+		// 			i++;
+		// 		}
+		// 	}
+    
+
+		// 	if( j<18 )
+		// 	{
+		// 		if( SPI_I2S_GetFlagStatus( SPI1, SPI_I2S_FLAG_RXNE ) != RESET )
+		// 		{
+		// 			RxData[j] = SPI_I2S_ReceiveData( SPI1 );
+		// 			j++;
+		// 		}
+		// 	}
+		// }
+
+		// value = memcmp( TxData, RxData, Size );
+
+		// if( value == 0 )
+		// {
+		// 	printf( "Same\r\n" );
+		// }
+		// else
+		// {
+		// 	printf( "Different\r\n" );
+		// }
+        // }
+		// i=0;
+	    // j=0;
+	}
 }
